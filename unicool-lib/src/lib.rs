7c9735253dd36a09689cd03bab159901253288f6
okno <paul.delafosse@protonmail.com>
2019-07-13 09:09:29 +0200

    remove curly brace from format and split tests

--- unicool-lib/src/lib.rs
+++ unicool-lib/src/lib.rs
@@ -9,13 +9,13 @@ pub fn convert_non_ascii_to_unicode(input: &str) -> Result<String, FromUtf8Error
     let writer: &mut dyn Write = &mut buffer;
 
     let mut pos = 0;
-    while pos < input.len() -1 {
+    while pos < input.len() - 1 {
         match bytes[pos].is_ascii() {
             true => {
                 writer.write(&bytes[pos..pos + 1]).unwrap();
             }
             false => {
-                let unicode = format!("\\u{{{:x}{:x}}}", &bytes[pos], &bytes[pos+1]);
+                let unicode = format!("\\u{:x}{:x}", &bytes[pos], &bytes[pos + 1]);
                 writer.write(unicode.as_bytes()).unwrap();
             }
         };
@@ -30,9 +30,18 @@ mod tests {
     use crate::convert_non_ascii_to_unicode;
 
     #[test]
-    fn escape_unicode() {
-        let input = "<p>Félicitations, votre compte Exacode à bien été créé !</p>\n\n<p>Pour finaliser votre inscription, il vous suffit de de cliquer sur le lien suivant afin de créer votre mot de passe :</p>\n\n<p><a href=\"{0}\">Créer un mot de passe</a></p>\n\n<p>Exacode</p>";
-        let expected = "<p>F\\u{c3a9}\\u{a96c}licitations, votre compte Exacode \\u{c3a0}\\u{a020} bien \\u{c3a9}\\u{a974}t\\u{c3a9}\\u{a920} cr\\u{c3a9}\\u{a9c3}\\u{c3a9}\\u{a920} !</p>\n\n<p>Pour finaliser votre inscription, il vous suffit de de cliquer sur le lien suivant afin de cr\\u{c3a9}\\u{a965}er votre mot de passe :</p>\n\n<p><a href=\"{0}\">Cr\\u{c3a9}\\u{a965}er un mot de passe</a></p>\n\n<p>Exacode</p>";
-        assert_eq!(convert_non_ascii_to_unicode(input).unwrap(), expected) ;
+    fn single_characters() {
+        assert_eq!(convert_non_ascii_to_unicode("à").unwrap(), "\\uc3a0");
+        assert_eq!(convert_non_ascii_to_unicode("é").unwrap(), "\\uc3a9");
+        assert_eq!(convert_non_ascii_to_unicode("û").unwrap(), "\\uc3bb");
+        assert_eq!(convert_non_ascii_to_unicode("ç").unwrap(), "\\uc3a7");
+        assert_eq!(convert_non_ascii_to_unicode("Ç").unwrap(), "\\uc387");
+    }
+
+    #[test]
+    fn test_complete_sentence() {
+        assert_eq!(convert_non_ascii_to_unicode("Ça, ça c'est vraiment toi!").unwrap(),
+                   "\\uc387\\u8761a, \\uc3a7\\ua761a c\'est vraiment toi");
+
     }
 }
